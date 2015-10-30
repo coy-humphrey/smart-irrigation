61d069f06d8161a368d7c5f3f03f6f84884209ee
Coy Humphrey <cmhumphr@ucsc.edu>
2015-10-30 10:49:52 -0700

    Updated ConfigGen with generic/descriptive default values

--- Backend/Prototypes/ConfigGen.py
+++ Backend/Prototypes/ConfigGen.py
@@ -9,19 +9,19 @@ config = ConfigParser.RawConfigParser()
 # Create section containing MySQL data
 config.add_section('MySQL')
 # Username and password for MySQL server
-config.set('MySQL', 'user', 'smart_irrigation')
-config.set('MySQL', 'password', '3Kv32B8JEliX0Qhk')
+config.set('MySQL', 'user', 'user_name')
+config.set('MySQL', 'password', 'password')
 # Address of server
-config.set('MySQL', 'host', 'smart-irrigation.canqqif3vrwd.us-west-1.rds.amazonaws.com')
+config.set('MySQL', 'host', 'address_of_server.com')
 # Name of database and table containing smart irrigation information
-config.set('MySQL', 'database', 'smart_irrigation')
-config.set('MySQL', 'table', 'entry')
+config.set('MySQL', 'database', 'database_name')
+config.set('MySQL', 'table', 'table_name')
 
 # Section containing all the fields that we will store in the database
 # Will be in format "fieldname = SQL Data Type"
 config.add_section('fields')
-config.set('fields', 'time', 'TIMESTAMP')
-config.set('fields', 'moisture_sensor_1', 'DOUBLE')
+config.set('fields', 'time_field', 'TIMESTAMP')
+config.set('fields', 'integer_field', 'INT')
 
 # Writing our configuration file to 'example.cfg'
 with open('config', 'wb') as configfile:
