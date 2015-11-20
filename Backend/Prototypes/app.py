51dcc9d5ecb9425d46e5489b43b703128383eb77
Coy Humphrey <cmhumphr@ucsc.edu>
2015-11-19 23:35:09 -0800

    Folded GetFieldBetweenKey into newer codebase

--- Backend/Prototypes/app.py
+++ Backend/Prototypes/app.py
@@ -1,17 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_restful import reqparse, abort, Api, Resource
 from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 import ConfigParser
 import MySQLdb
 import datetime
+import os
 
 
-app = Flask(__name__)
+application = Flask(__name__)
 auth = HTTPBasicAuth()
-api = Api(app)
+api = Api(application)
 
 config = ConfigParser.RawConfigParser()
-config.read("config")
+config.read('config')
+#config = ConfigParser.ConfigParser()
+#configdir = os.path.dirname(os.path.realpath(__file__))
+#configpath = os.path.join(configdir, "config", "configAPI.ini")
+#config.read(configpath)
 
 # Pull settings from the MySQL section of config file
 sql_config = {
@@ -21,25 +27,28 @@ sql_config = {
   'db': config.get('MySQL', 'database'),
 }
 
-users = {
-	"testuser" : "password"
-}
-
-table = config.get("MySQL", 'table')
+# Basic test to see if authentication is working
+class Welcome(Resource):
+    decorators = [auth.login_required]
+    def get(self):
+        return "All Hail %s!" % (auth.username())
 
 # Arguments will be field: the name of the field to pull (specify field multiple times to pull multiple fields)
 # Start and end, the start and end dates. Results will be in the form of
 # a list of all times and fields (in a dict) that fall between the start and end date
 # Times and fields will be expressed as Strings
-# Sample call: /get_field_between_time?field=temp&start=%222014-10-06_06:27:29%22&end=%222015-12-22_14:04:29%22
-# Sample call (pulling multiple fields): /get_field?field=s1&field=s2&start=%222014-10-06_06:27:29%22&end=%222015-12-22_14:04:29%22
+# Sample call: /get_field?field=temp&start=%222014-10-06_06:27:29%22&end=%222015-12-22_14:04:29%22
+# Sample call (pulling multiple fields): /get_field?table=entry&field=s1&field=s2&start=%222014-10-06_06:27:29%22&end=%222015-12-22_14:04:29%22
 class GetFieldBetweenTime(Resource):
+    decorators = [auth.login_required]
+
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('field', required=True, action='append')
         self.parser.add_argument('start', required=True)
         self.parser.add_argument('end', required=True)
-        super(GetFieldBetweenTime, self).__init__()
+        self.parser.add_argument('table', required=True)
+        super(GetField, self).__init__()
 
     def validate(self, args):
         fields_valid = validateFields (args['field'])
@@ -57,7 +66,7 @@ class GetFieldBetweenTime(Resource):
         if not valid[0]:
             abort(400, message=valid[1])
         # Form query using the received arguments
-        query = ("SELECT time, {} FROM {} where time BETWEEN {} and {}".format(",".join(args['field']), table, args['start'], args['end']))
+        query = ("SELECT time, {} FROM {} where time BETWEEN {} and {}".format(",".join(args['field']), args['table'], args['start'], args['end']))
         # Print for debugging
         print query
 
@@ -66,7 +75,6 @@ class GetFieldBetweenTime(Resource):
         print result
 
         return result
-
 
 # Arguments will be field: the name of the field to pull (specify field multiple times to pull multiple fields)
 # Start and end, the start and end values. Results will be in the form of
@@ -81,6 +89,7 @@ class GetFieldBetweenKey(Resource):
         self.parser.add_argument('start', required=True)
         self.parser.add_argument('end', required=True)
         self.parser.add_argument('key', required=True)
+        self.parser.add_argument('table', required=True)
         super(GetFieldBetweenKey, self).__init__()
 
     def validate(self, args):
@@ -99,7 +108,7 @@ class GetFieldBetweenKey(Resource):
         if not valid[0]:
             abort(400, message=valid[1])
         # Form query using the received arguments
-        query = ("SELECT {}, {} FROM {} where {} BETWEEN {} and {}".format(args['key'], ",".join(args['field']), table, args['key'], args['start'], args['end']))
+        query = ("SELECT {}, {} FROM {} where {} BETWEEN {} and {}".format(args['key'], ",".join(args['field']), args['table'], args['key'], args['start'], args['end']))
         # Print for debugging
         print query
 
@@ -109,17 +118,19 @@ class GetFieldBetweenKey(Resource):
 
         return result
 
-
 # Given any number of fields, a start date and an end date, returns the average of each field
 # between the start and end date.
 # Example call: /get_average?field=temp&start=%222014-10-06_06:27:29%22&end=%222015-12-22_14:04:29%22
 class GetAvg(Resource):
+    decorators = [auth.login_required]
+
     def __init__(self):
         # Set up the RequestParser
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('field', required=True, action='append')
         self.parser.add_argument('start', required=True)
         self.parser.add_argument('end', required=True)
+        self.parser.add_argument('table', required=True)
         super(GetAvg, self).__init__()
 
     def validate(self, args):
@@ -143,7 +154,7 @@ class GetAvg(Resource):
 
         # Form query using the received arguments, in this case putting field arguments in the AVG function
         fields = map (lambda s: "AVG({0}) as {0}".format(s), args['field'])
-        query = ("SELECT {} FROM {} where time BETWEEN {} and {}".format(",".join(fields), table, args['start'], args['end']))
+        query = ("SELECT {} FROM {} where time BETWEEN {} and {}".format(",".join(fields), args['table'], args['start'], args['end']))
         # Print for debugging
         print query
 
@@ -158,42 +169,32 @@ class GetAvg(Resource):
 
         return result
 
-
 #Authentication Functions, first hardcoded later DB interacted
-@auth.get_password
 def get_pw(username):
-	#if username in users :
-	#	return users.get(username)
+        
+    userQry = performQueryRaw("SELECT password FROM userDB WHERE username='%s'" % username);
+    if not userQry :
+        return ""
+        
+    return userQry[0]["password"]
+        
+ #flask function to verify pw.
+@auth.verify_password
+def verify_pw(username, password):
+    return check_pw(username, password)
 
-	userQry = performQueryRaw("SELECT password FROM userDB WHERE username='%s'" % username);
-	if not userQry :
-		return ""
+#hashes a password for storage using SHA1 + salt
+def hashed_pw(password) :
+    return generate_password_hash(password)
 
-	return userQry[0]["password"]
-
-@app.route('/')
-@auth.login_required
-def index():
-	return "All Hail %s!" % (auth.username())
-
-#for hashing if/when deemed neccessary
-#@auth.hash_password
-#def hash_pw (username, password) :
-#	get_salt(username)
-#	return hash(password,salt)
-#
-#	OR
-#
-#@auth.verify_password
-#def verify_pw(username, password) :
-#	return ourVerificationFunction(usr,pw)
+#user Werkzeug function to check unhashed password against hashed.
+def check_pw(username, password) :
+    return check_password_hash(get_pw(username), password)
 
 def performQuery (query):
     """Perform query and convert results into JSONable objects
-
     Arguments:
     query - A string containing a valid MySQL query
-
     Performs the query given in the string query. Converts all of the results of the query into objects that
     can be converted to JSON.
     """
@@ -206,10 +207,10 @@ def performQuery (query):
             functs = {"TIMESTAMP": str, "INT": int, "DOUBLE":float, None: str}
             # Try to determine data type
             key_type = None
-            try:
+            try: 
                 key_type = config.get('fields', key).upper()
             except:
-                key_type = None
+                key_type = None 
             # Apply conversion function
             row[key] = (functs[key_type])(row[key])
 
@@ -218,10 +219,8 @@ def performQuery (query):
 
 def performQueryRaw (query):
     """Perform query and return raw results
-
     Arguments:
     query - A string containing a valid MySQL query
-
     Performs the query given in the string query. Results may be in MySQL types that will fail to convert
     to JSON.
     """
@@ -251,11 +250,6 @@ def validateFields (fields):
             return False
     return True
 
-def validateKey (key):
-    if key.lower() not in [x.lower() for x in config.options("fields")]:
-        return False
-    return True
-
 def validateDates (dates):
     for d in dates:
         if d.startswith('"') and d.endswith('"'):
@@ -268,10 +262,13 @@ def validateDates (dates):
     return True
 
 ## Actually setup the Api resource routing here
-api.add_resource(GetFieldBetweenTime, '/get_field_between_time')
-api.add_resource(GetFieldBetweenKey, '/get_field_between_key')
+api.add_resource(Welcome, '/')
+
+api.add_resource(GetFieldBetweenTime, '/get_field', 'get_field_between_time')
 api.add_resource(GetAvg, '/get_average')
+api.add_resource(GetFieldBetweenKey, '/get_field_between_key')
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    application.run(debug=True)
