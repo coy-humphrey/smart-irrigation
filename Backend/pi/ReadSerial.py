ec51408326f1d8d475e2975840dca986bf536bfd
Coy Humphrey <cmhumphr@ucsc.edu>
2015-11-24 18:16:36 -0800

    Updated ReadSerial comments

--- Backend/pi/ReadSerial.py
+++ Backend/pi/ReadSerial.py
@@ -8,46 +8,56 @@ import sys
 import select
 
 def get_devices():
-	config = ConfigParser.RawConfigParser( )
-	config.read( 'serial_devices.ini' )
-	results = {}
-	for device in config.options('devices'):
-		if config.get ('devices', device).lower() == "serial":
-			stream = serial.Serial(device, 9600)
-		else:
-			stream = sys.stdin
-		results[stream] = [x.strip() for x in config.get('format', device).split(',')]
-	return results
+    # Config file lists all devices to read from as well as the sensor readings to expect
+    config = ConfigParser.RawConfigParser( )
+    config.read( 'serial_devices.ini' )
+    results = {}
+
+    for device in config.options('devices'):
+        # Ultimately, all devices will be serial devices. Stdin is supported for testing reasons
+        if config.get ('devices', device).lower() == "serial":
+            # Here we assume that 9600 baud will work. It may be a good idea to support manually specifying a baud rate.
+            stream = serial.Serial(device, 9600)
+        else:
+            stream = sys.stdin
+
+        # Result is dictionary
+        # Handle supporting readline() as key
+        # list of field names of sensor readings as value
+        results[stream] = [x.strip() for x in config.get('format', device).split(',')]
+    return results
 
 def parseLine (device, device_dict):
-	line = device.readline()
-	# In future iterations, this should cause some sort of error message and possibly an attempt to reconnect.
-	if not line: 
-		return False
-	# Devices give a tab separated list of sensor readings not including time
-	# So we generate the time
-	curr_time = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-	# We pull the sensor readings from the input and throw them in a list with time
-	results = [curr_time] + line.strip().split('\t')
-	# We get the row names of the sensors and append them to "time" the name of the time row
-	fields = ["time"] + device_dict[device]
-	# Then we put the readings in a dictionary with the row names as keys
-	results = dict(zip(fields, results))
-	# For now we print the dictionary, in the future we will push it to DB
-	print results
-	# Returning true tells the calling function that this stream is still open
-	return True
+    line = device.readline()
+    # In future iterations, this should cause some sort of error message and possibly an attempt to reconnect.
+    if not line: 
+        return False
+    # Devices give a tab separated list of sensor readings not including time
+    # So we generate the time
+    curr_time = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    # We pull the sensor readings from the input and throw them in a list with time
+    results = [curr_time] + line.strip().split('\t')
+    # We get the row names of the sensors and append them to "time" the name of the time row
+    fields = ["time"] + device_dict[device]
+    # Then we put the readings in a dictionary with the row names as keys
+    results = dict(zip(fields, results))
+    # For now we print the dictionary, in the future we will push it to DB
+    print results
+    # Returning true tells the calling function that this stream is still open
+    return True
 
 def main():
+    # Get dictionary with devices as keys and field names of sensor readings as values
     device_dict = get_devices()
+    # Get list of devices for select to monitor
     devices = device_dict.keys()
 
-    # While there are still open streams
+    # While there are still open devices
     while devices:
-    	# Wait for a stream to have data ready
-    	readable, _, _ = select.select(devices, [], [])
-    	# Then, parse a line from each ready stream. If a stream is closed, remove it from list
-    	devices = [x for x in readable if parseLine(x, device_dict)]
+        # Wait for a device to have data ready
+        readable, _, _ = select.select(devices, [], [])
+        # Then, parse a line from each ready device. If a device is closed, remove it from list
+        devices = [x for x in readable if parseLine(x, device_dict)]
 
 
 if __name__ == '__main__':
